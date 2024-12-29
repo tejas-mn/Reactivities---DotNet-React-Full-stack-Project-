0b812fa16ec2dd49708342794ac916ebc0051d14
tejas-mn-alt <159873533+tejas-mn-alt@users.noreply.github.com>
2024-12-29 10:55:07 +0000

    Added Security Headers

--- DotNetReact/API/Program.cs
+++ DotNetReact/API/Program.cs
@@ -20,11 +20,50 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
+// prevent browsers from interpreting files as a different MIME type than what is specified by the server. This helps mitigate certain types of attacks, such as MIME sniffing,
+app.UseXContentTypeOptions();
+
+//Controls how much information is shared when navigating from your domain to an external site
+app.UseReferrerPolicy(opt => opt.NoReferrer());
+
+//blocking cross side scripting
+app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+
+// blocking iframe
+app.UseXfo(opt => opt.Deny());
+
+app.UseCsp(opt => opt
+    .BlockAllMixedContent()
+    // we're gonna allow below sources from our domain
+    .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com",
+    "sha256-yChqzBduCCi4o4xdbXRXh4U/t1rP4UUUMJt+rB+ylUI="))
+    .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+    .FormActions(s => s.Self())
+    .FrameAncestors(s => s.Self())
+    .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "https://www.facebook.com", "data:"))
+    .ScriptSources(s => s.Self()
+        .CustomSources(
+            "sha256-RZa9FeBeiqCM2+5jcE1mUKMkRZ69RNcOg/OAT6NWJ7Y=",
+            "sha256-rTuNa9UeAQ5B9gj9rLEY9243ZbVE6Sl++AiPas66JE4=",
+            "https://connect.facebook.net"
+        )
+    )
+);
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.Use(async (context, next) =>
+    {
+        // tells browsers to only interact with your site over HTTPS for a specified duration
+        context.Response.Headers.Append("Strict-Transport-Security", "max-age=31536000");
+        await next.Invoke();
+    });
 }
 
 app.UseCors("CorsPolicy");
